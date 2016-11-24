a1beb8ccd82c1f7b955a16f8d5351cde1c043c0f
Corin22225 <mrcoltj@gmail.com>
2016-11-24 09:32:53 -0500

    Initial commit

---
+++ src/main/java/me/corin22225/chatfilter/ChatFilter.java
@@ -0,0 +1,29 @@
+package me.corin22225.chatfilter;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatFilter extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage().toLowerCase();
+
+        for (String swears : this.getConfig().getStringList("filter")) {
+            if(message.contains(swears)) {
+                player.sendMessage(ChatColor.RED + "Please avoid saying swear words in chat.");
+                event.setCancelled(true);
+            }
+        }
+    }
+}
